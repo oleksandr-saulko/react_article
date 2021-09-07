fd23edd7dd2352e3231a1fbb79a64e57e02f9b69
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-09-07 15:27:50 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.6404bd952c80596ac185d6c5536ff2dc.js"
 );
 
 self.addEventListener('message', (event) => {
